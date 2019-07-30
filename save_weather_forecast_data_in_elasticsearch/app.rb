f5deb0bcce245477168e9231bd5021f42bd22051
hirontan <hirontan@sora.flights>
2019-07-30 14:00:15 +0900

    メッセージのフォーマットを作成

--- save_weather_forecast_data_in_elasticsearch/app.rb
+++ save_weather_forecast_data_in_elasticsearch/app.rb
@@ -1,10 +1,16 @@
 require 'json'
 
 def lambda_handler(event:, context:)
+  display_message(200, "OK")
+rescue => error
+  display_message(400, error.message)
+end
+
+def display_message(status_code, message)
   {
-    statusCode: 200,
+    statusCode: status_code,
     body: {
-      message: "Hello World!",
+      message: message
     }.to_json
   }
 end
